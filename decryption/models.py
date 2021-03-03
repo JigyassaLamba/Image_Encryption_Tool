15b62d69fe54f42d3dce5a5d87dceb73b6736dce
blurryfca3 <aadityamahadevan07@gmail.com>
2021-03-03 12:18:21 +0530

    Minor UI changes

--- decryption/models.py
+++ decryption/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class ImageDecryption(models.Model):
-    text = models.CharField(max_length = 10)
+    text = models.CharField(max_length = 20)
     image = models.ImageField(upload_to = 'img/%y')
 
     def __str__(self):
